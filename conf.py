f1fd1bdaad0f8f26dabbcd75d822e96bbed0fc61
algabsil <76498959+algabsil@users.noreply.github.com>
2022-07-27 21:50:38 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -16,16 +16,16 @@ import time
 
 
 # Data about this site
-BLOG_AUTHOR = "Your Name"  # (translatable)
-BLOG_TITLE = "Demo Site"  # (translatable)
+BLOG_AUTHOR = "Alessio"  # (translatable)
+BLOG_TITLE = "Alessio's Blog"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
-SITE_URL = "https://example.com/"
+SITE_URL = "alessiosblog.netlify.app"
 # This is the URL where Nikola's output will be deployed.
 # If not set, defaults to SITE_URL
 # BASE_URL = "https://example.com/"
 BLOG_EMAIL = "joe@demo.site"
-BLOG_DESCRIPTION = "This is a demo site for Nikola."  # (translatable)
+BLOG_DESCRIPTION = "This is a demo site for my future blog"  # (translatable)
 
 # Nikola is multilingual!
 #
